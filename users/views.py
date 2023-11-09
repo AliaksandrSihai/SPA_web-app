e5e19bea50734b6b47f9a0190cdae5e55e0a9dce
Aliaksandr <sigai.aleksandr@mail.ru>
2023-11-09 17:15:44 +0200

    User serializer, permissions

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from habit.permissions import IsModerator, IsOwner
+from users.models import User
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """ CRUD для пользователя """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
